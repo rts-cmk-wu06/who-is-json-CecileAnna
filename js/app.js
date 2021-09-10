8b8b69d16d82669fa7bcf790e8c11ab840eafe66
CecileAnna <6263325@rts-365.dk>
2021-09-10 13:11:38 +0200

    print classmembers works

--- js/app.js
+++ js/app.js
@@ -1,71 +1,31 @@
 fetch("./js/team.json")
     .then(response => response.json())
-    .then(data => console.log(data));
+    .then(webdevClass => {
 
-// console.log(data[0]["classMembers"][2]['firstName']);
+        console.log(webdevClass);
 
-let webdevClass = data;
+        // print classmates
+
+        let titleH1 = document.querySelector('#class');
+        let subtitleH2 = document.querySelector('#school');
+        let classMembersH3 = document.querySelector('#class-members');
+        let classMembers = document.querySelector('#memberDetails');
+
+        titleH1.textContent = webdevClass['class'];
+        subtitleH2.textContent = webdevClass['school'];
+        classMembersH3.textContent = "Detaljer om klassens studerende:";
+
+        for (let i = 0; i < webdevClass.classMembers.length; i++) {
+
+            let liItem = document.createElement('li');
+            liItem.textContent = `${webdevClass.classMembers[i]["firstName"]} ${webdevClass.classMembers[i]["lastName"]}`;
+            classMembers.appendChild(liItem);
+
+        };
+
+    });
 
 
-
-// const webdevClass = {
-//     "class": "WU06",
-//     "school": "RTS",
-//     "teacher": "Troels",
-//     "classMembers": [{
-//             "firstName": "Vagn",
-//             "lastName": "Sej",
-//             "age": 70,
-//             "hairColor": "grey",
-//             "shoeSize": 45,
-//         },
-//         {
-//             "firstName": "Christian",
-//             "lastName": "Cool",
-//             "age": 28,
-//             "hairColor": "blonde",
-//             "shoeSize": 44,
-//         },
-//         {
-//             "firstName": "Özlem",
-//             "lastName": "Awesome",
-//             "age": 26,
-//             "hairColor": "black",
-//             "shoeSize": 39,
-
-//         },
-//         {
-//             "firstName": "William",
-//             "lastName": "Wizard",
-//             "age": 17,
-//             "hairColor": "brown",
-//             "shoeSize": 43,
-
-//         }
-//     ]
-// };
-
-// console.log(webdevClass["classMembers"][1]["firstName"]);
-
-for (let i = 0; i < webdevClass["classMembers"].length; i++) {
-
-    // console.log(webdevClass["classMembers"][i]["firstName"], webdevClass["classMembers"][i]["lastName"]);
-
-    console.log(`${webdevClass["classMembers"][i]["firstName"]} ${webdevClass["classMembers"][i]["lastName"]}`);
-
-};
-
-// print classmates
-
-let titleH1 = document.querySelector('#class');
-let subtitleH2 = document.querySelector('#school');
-let classMembersH3 = document.querySelector('#class-members');
-
-let classMembers = document.querySelector('#memberDetails');
-
-titleH1.textContent = webdevClass['class'];
-subtitleH2.textContent = webdevClass['school'];
-classMembersH3.textContent = "Detaljer om klassens studerende:";
 
 // classMembersListUl.innerHTML = JSON.stringify(webdevClass);
 
@@ -75,12 +35,11 @@ classMembersH3.textContent = "Detaljer om klassens studerende:";
 
 // prettyFormatSpaceSize4();
 
-for (let i = 0; i < webdevClass["classMembers"].length; i++) {
+// for (let i = 0; i < webdevClass["classMembers"].length; i++) {
 
-    let listItem = document.createElement('li');
-    listItem.textContent = `${webdevClass["classMembers"][i]["firstName"]} ${webdevClass["classMembers"][i]["lastName"]}`;
-    classMembers.appendChild(listItem);
+//     let liItem = document.createElement('li');
+//     liItem.textContent = `${webdevClass["classMembers"][i]["firstName"]} ${webdevClass["classMembers"][i]["lastName"]}`;
+//     classMembers.appendChild(liItem);
 
-    // console.log(`${webdevClass["classMembers"][i]["firstName"]} ${webdevClass["classMembers"][i]["lastName"]}`);
 
-};
+// };
